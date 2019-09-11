c62a057460263a2d625e11ad2de8ea74e1492f11
Wiedemann Matthias <Matthias.Wiedemann@swisslife.de>
2019-09-11 15:23:00 +0200

    using bean validation 2.0 and pom cleanup

--- src/test/java/com/github/mwiede/feign/validation/Example.java
+++ src/test/java/com/github/mwiede/feign/validation/Example.java
@@ -1,19 +1,20 @@
 package com.github.mwiede.feign.validation;
 
-import java.util.List;
+import com.google.gson.*;
+import feign.Param;
+import feign.RequestLine;
+import feign.gson.GsonDecoder;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import feign.Feign;
-import feign.InvocationHandlerFactory;
-import feign.Param;
-import feign.RequestLine;
-import feign.gson.GsonDecoder;
+import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 /**
  * @author Matthias Wiedemann (mwiede at gmx.de)
@@ -26,6 +27,9 @@ public class Example {
         @Valid
         List<Contributor> contributors(@Param("owner") String owner, @Param("repo") String repo);
 
+        @RequestLine("GET /repos/{owner}/{repo}")
+        @Valid
+        Repo repo(@Param("owner") String owner, @Param("repo") String repo);
     }
 
     static class Contributor {
@@ -35,19 +39,30 @@ public class Example {
         int contributions;
     }
 
+    static class Repo {
+        @NotNull
+        @Future
+        ZonedDateTime created_at;
+        String name;
+    }
+
+    static class DateTimeDeserializer implements JsonDeserializer<ZonedDateTime> {
+        public ZonedDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString());
+        }
+    }
     public static void main(String... args) {
 
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Gson gson = new GsonBuilder().registerTypeAdapter(ZonedDateTime.class, new DateTimeDeserializer()).create();
 
         GitHub github = ExtendedFeign.builder(validator)//
-                .decoder(new GsonDecoder()).target(GitHub.class, "https://api.github.com");
+                .decoder(new GsonDecoder(gson)).target(GitHub.class, "https://api.github.com");
 
         // Fetch and print a list of the contributors to this library.
         try {
-            List<Contributor> contributors = github.contributors("OpenFeign", "feign");
-            for (Contributor contributor : contributors) {
-                System.out.println(contributor.login + " (" + contributor.contributions + ")");
-            }
+            Repo repo = github.repo("OpenFeign", "feign");
         } catch (ConstraintViolationException ex) {
             ex.getConstraintViolations().forEach(System.out::println);
         }
